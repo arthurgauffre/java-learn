eb8bd3fc0b7f463fc1cbbdaf46c02f3763ae3401
Arthur <arth.gauffre@gmail.com>
2022-07-21 15:37:57 +0200

    Java début

---
+++ P1.java
@@ -0,0 +1,10 @@
+public class P1 {
+    public static void main(String[] arg){
+
+        String text = "Salut";
+        int age = 16;
+        age++;   
+        System.out.println(text + " j'ai " + age);
+    }
+    
+}
---
+++ moyenne.java
@@ -0,0 +1,9 @@
+public class moyenne {
+    public static void main(String[] arg){
+        int note1 = 12;
+        int note2 = 15;
+        int note3 = 16;
+        int calcul = (note1 + note2 + note3) / 3;
+        System.out.println(calcul);
+    }
+}
