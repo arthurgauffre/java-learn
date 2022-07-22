440689d59ec42f91ad566e8969724907517f1751
Arthur <arth.gauffre@gmail.com>
2022-07-22 16:44:29 +0200

    java

---
+++ SWITCH.java
@@ -0,0 +1,20 @@
+public class SWITCH {
+    public static void main(String[] arg){
+
+        int age = 10;
+
+        switch(age){
+            
+            case 10:
+                System.out.println("la personne est jeune!");
+            break;
+
+            case 11:
+                System.out.println("la personne est un peu plus agé");
+            break;
+
+            default: System.out.println("Cette age n'a pas de message");
+        }
+    }
+    
+}
---
+++ si.java
@@ -0,0 +1,11 @@
+public class si {
+    public static void main(String[] arg){
+        int money = 1000;
+        if(money == 2000 || money <= 5000){
+            System.out.println("les deux valeurs sont bien les mêmes");
+        } else {
+            System.out.println("les deux valeurs ne sont pas les mêmes");
+        }
+    }
+    
+}
